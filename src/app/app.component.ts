75bcd747431c46784e5cb2ed6959de297e36ed97
nkg-laufire <nkg@laufire.in>
2021-12-01 16:05:38 +0530

    Experimented with subscribe and unsuscribe.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  count: number = 0;
   items = ['item1', 'item2', 'item3', 'item4'];
+
+  constructor(private store: Store<{ root: any}>) {
+  }
+
+  ngOnInit() {
+    setInterval(() => this.store.dispatch({ type: '[Movies Page] Load Movies' }), 1000);
+    this.store.select('root', 'count').subscribe((count) => this.count = count);
+  }
 
   addItem(newItem: string) {
     this.items.push(newItem);
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -7,12 +7,19 @@ import { Store } from '@ngrx/store';
 })
 export class MoviesComponent {
   movies: any;
- 
+  subscription: any;
+
   constructor(private store: Store<{ root: any}>) {
-    this.movies = store.select('root', 'movies');
   }
- 
+
   ngOnInit() {
+    this.subscription = this.store.select('root', 'movies').subscribe((movies) =>
+      this.movies = movies
+    );
     this.store.dispatch({ type: '[Movies Page] Load Movies' });
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 }
